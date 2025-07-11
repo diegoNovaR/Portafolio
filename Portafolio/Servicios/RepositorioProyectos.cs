4e534ec14e3bad0d9b893e4675bf8c4dbbac850f
diegoNovaR <diegonova368@gmail.com>
2025-07-11 16:39:35 -0500

    Realizando ajustes a los cards del carrusel

--- Portafolio/Servicios/RepositorioProyectos.cs
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -41,6 +41,17 @@ namespace Portafolio.Servicios
                     UrlGit = "https://github.com/diegoNovaR/Portafolio.git",
                     Url = "#",
                     Tecnologias = "C#, Blazor, ASP.NET, HTML, CSS, Bootstrap"
+                },
+                 new ProyectoDTO
+                {
+                    Titulo = "Cafeteria",
+                    DescripcionCorta = "Es una presentacion de una cafeteria realizada para reforzar las habilidades que aprendí",
+                    Descripcion = "Con este proyecto un cliente puede iniciar sesión, añadir compras a su carrito, puede eliminar las mismas compras y eliminar su sesión."
+                    + "Para este proyecto utilice C# como lenguaje de programación ASP.NET, Framework Blazor aplicando MVC",
+                    ImagenURL = "/Imagenes/integra.jpg",
+                    UrlGit = "https://github.com/diegoNovaR/Portafolio.git",
+                    Url = "#",
+                    Tecnologias = "C#, Blazor, ASP.NET, HTML, CSS, Bootstrap"
                 }
             };
         }
